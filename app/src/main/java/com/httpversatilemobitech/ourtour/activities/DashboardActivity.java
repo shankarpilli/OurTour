a2e62960f52a42852d40eee4bca5f731f3426544
manikanta.chadaram1992@gmail.com <manikanta.chadaram@sparity.com>
2016-10-14 23:45:38 +0530

    left menu items added to listview and navigation is added

--- app/src/main/java/com/httpversatilemobitech/ourtour/activities/DashboardActivity.java
+++ app/src/main/java/com/httpversatilemobitech/ourtour/activities/DashboardActivity.java
@@ -1,20 +1,123 @@
 package com.httpversatilemobitech.ourtour.activities;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.httpversatilemobitech.ourtour.R;
+import com.httpversatilemobitech.ourtour.adapters.LeftMenuAdapter;
+import com.httpversatilemobitech.ourtour.fragments.AboutUsFragment;
+import com.httpversatilemobitech.ourtour.fragments.ContctUsFragment;
+import com.httpversatilemobitech.ourtour.fragments.DiclaimerFragment;
+import com.httpversatilemobitech.ourtour.fragments.HelpFragment;
 import com.httpversatilemobitech.ourtour.fragments.HomeFragment;
+import com.httpversatilemobitech.ourtour.fragments.TermsAndConditionsFragment;
 import com.httpversatilemobitech.ourtour.utils.Utility;
+
+import java.util.ArrayList;
 
 /**
  * Created by Rev's Nani on 13-10-2016.
  */
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private DrawerLayout drawerHomeLayout;
+    private ImageView imgLeftDrawerIcon;
+    private ListView lvLeftDrawer;
+
+    private ArrayList<String> mSideMenuItemNamesList;
+    private int[] mSideMenuItemIconsList = {R.drawable.side_menu_home,R.drawable.side_menu_abou_us,
+            R.drawable.side_menu_contact_us,R.drawable.side_menu_terms_conditions,
+            R.drawable.side_menu_diclaimer,R.drawable.side_menu_help};
+
+    private LeftMenuAdapter mLeftMenuAdapter= null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        initUI();
         Utility.navigateDashBoardFragment(new HomeFragment(), HomeFragment.TAG, null, DashboardActivity.this);
+    }
+
+    private void initUI() {
+
+        drawerHomeLayout = (DrawerLayout)findViewById(R.id.drawer_home_layout);
+        imgLeftDrawerIcon = (ImageView)findViewById(R.id.iv_left_drawer_icon);
+        lvLeftDrawer = (ListView)findViewById(R.id.lv_left_drawer);
+
+        setData();
+    }
+
+    private void setData() {
+        mSideMenuItemNamesList = new ArrayList<>();
+        mSideMenuItemNamesList.add("Home");
+        mSideMenuItemNamesList.add("About Us");
+        mSideMenuItemNamesList.add("Contact us");
+        mSideMenuItemNamesList.add("Terms and conditions");
+        mSideMenuItemNamesList.add("Disclaime");
+        mSideMenuItemNamesList.add("Help");
+
+        mLeftMenuAdapter = new LeftMenuAdapter(this,mSideMenuItemNamesList,mSideMenuItemIconsList);
+
+        lvLeftDrawer.setAdapter(mLeftMenuAdapter);
+        setHeaderFooterToListView(lvLeftDrawer);
+        
+        lvLeftDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,final int position, long id) {
+                drawerHomeLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        drawerHomeLayout.closeDrawers();
+                        navigateScreens(position);
+                    }
+                }, 300);
+            }
+        });
+    }
+
+    private void navigateScreens(int position) {
+        switch (position) {
+            case 0:
+                Utility.navigateDashBoardFragment(new HomeFragment(), HomeFragment.TAG, null, DashboardActivity.this);
+                break;
+            case 1:
+                Utility.navigateDashBoardFragment(new AboutUsFragment(), AboutUsFragment.TAG, null, DashboardActivity.this);
+                break;
+            case 2:
+                Utility.navigateDashBoardFragment(new ContctUsFragment(), ContctUsFragment.TAG, null, DashboardActivity.this);
+                break;
+            case 3:
+                Utility.navigateDashBoardFragment(new TermsAndConditionsFragment(), TermsAndConditionsFragment.TAG, null, DashboardActivity.this);
+                break;
+            case 4:
+                Utility.navigateDashBoardFragment(new DiclaimerFragment(), DiclaimerFragment.TAG, null, DashboardActivity.this);
+                break;
+            case 5:
+                Utility.navigateDashBoardFragment(new HelpFragment(), HelpFragment.TAG, null, DashboardActivity.this);
+                break;
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.drawer_home_layout:
+                drawerHomeLayout.openDrawer(GravityCompat.START);
+                break;
+        }
+    }
+    private void setHeaderFooterToListView(ListView list_view) {
+        LinearLayout layout_list_header = (LinearLayout) getLayoutInflater().inflate(R.layout.
+                layout_list_header, null);
+        list_view.addHeaderView(layout_list_header);
+
     }
 }
---
+++ app/src/main/java/com/httpversatilemobitech/ourtour/adapters/LeftMenuAdapter.java
@@ -0,0 +1,72 @@
+package com.httpversatilemobitech.ourtour.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.httpversatilemobitech.ourtour.R;
+import com.httpversatilemobitech.ourtour.activities.DashboardActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Rev's Nani on 14-10-2016.
+ */
+
+public class LeftMenuAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+    ArrayList<String> mSideMenuItemNamesList;
+    int[] mSideMenuItemIconsList;
+
+    public LeftMenuAdapter(Context mContext, ArrayList<String> mSideMenuItemNamesList, int[] mSideMenuItemIconsList) {
+        this.mContext = mContext;
+        this.mSideMenuItemIconsList = mSideMenuItemIconsList;
+        this.mSideMenuItemNamesList = mSideMenuItemNamesList;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+
+    @Override
+    public int getCount() {
+        return mSideMenuItemNamesList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LeftMenuItemHolder mLeftMenuItemHolder = null;
+
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(R.layout.item_left_menu,
+                    null);
+            mLeftMenuItemHolder = new LeftMenuItemHolder();
+            mLeftMenuItemHolder.txt_left_drawer_text = (TextView) convertView.findViewById(R.id.txt_left_drawer_text);
+            mLeftMenuItemHolder.iv_left_drawer_icon = (ImageView) convertView.findViewById(R.id.iv_left_drawer_icon);
+            convertView.setTag(mLeftMenuItemHolder);
+        } else {
+            mLeftMenuItemHolder = (LeftMenuItemHolder) convertView.getTag();
+        }
+        mLeftMenuItemHolder.txt_left_drawer_text.setText(mSideMenuItemNamesList.get(position));
+        mLeftMenuItemHolder.iv_left_drawer_icon.setImageResource(mSideMenuItemIconsList[position]);
+        return convertView;
+    }
+    private class LeftMenuItemHolder {
+        private TextView txt_left_drawer_text;
+        private ImageView iv_left_drawer_icon;
+    }
+}
