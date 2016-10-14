418fef5d756fbe4b81bafb1fe91404eaf52f5b86
Manikanta Chadaram <manikanta.chadaram@sparity.com>
2016-10-14 10:54:07 +0530

    1st commit

---
+++ app/src/main/java/com/httpversatilemobitech/ourtour/fragments/ContctUsFragment.java
@@ -0,0 +1,44 @@
+package com.httpversatilemobitech.ourtour.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.httpversatilemobitech.ourtour.R;
+import com.httpversatilemobitech.ourtour.activities.DashboardActivity;
+
+/**
+ * Created by Rev's Nani on 13-10-2016.
+ */
+public class ContctUsFragment extends Fragment {
+
+    public static final String TAG = "ContctUsFragment";
+    private DashboardActivity mParent;
+
+    private Toolbar mToolbar;
+    private View rootView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mParent = (DashboardActivity) getActivity();
+        mToolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (mToolbar != null) {
+            mToolbar.setVisibility(View.VISIBLE);
+        }
+        rootView = inflater.inflate(R.layout.fragment_contact_us, container, false);
+
+        return rootView;
+    }
+}
